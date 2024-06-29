d067dbb70679e47d4e765a015e6e62fe746ac03c
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2024-06-28 21:20:25 -0300

    fourth commit

--- feriados/core/views.py
+++ feriados/core/views.py
@@ -3,9 +3,8 @@ from core.models import FeriadoModel
 from datetime import date
 
 def feriados(request):
-    hoje = date.today()
-    feriado_encontrado = FeriadoModel.objects.filter(data=hoje)
-    if len(feriado_encontrado) == 0:
-        return render(request, 'feriados.html')
-    contexto = {'feriado':str(feriado_encontrado[0])}
+    contexto = {}
+    feriado_encontrado = FeriadoModel.objects.filter(data=date.today())
+    if len(feriado_encontrado) == 1:
+        contexto = {'feriado':str(feriado_encontrado[0])}
     return render(request, 'feriados.html', contexto)
